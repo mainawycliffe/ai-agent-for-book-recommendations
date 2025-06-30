5f57e6a6758df34b449e2b53cd087e78dde48ccb
Maina Wycliffe <wmmaina7@gmail.com>
2025-06-30 20:06:05 +0200

    Refactor code structure for improved readability and maintainability

--- apps/webapp/src/environments/environment.ts
+++ apps/webapp/src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBPyQxfUqbZ3LcehijHEWUsTQN-lDGJ-ok',
-    authDomain: 'angular-genkit-demo.firebaseapp.com',
-    databaseURL: 'https://angular-genkit-demo-default-rtdb.firebaseio.com',
-    projectId: 'angular-genkit-demo',
-    storageBucket: 'angular-genkit-demo.firebasestorage.app',
-    messagingSenderId: '603362092209',
-    appId: '1:603362092209:web:a014f398175c653b60ee90',
+    apiKey: 'AIzaSyB5XYrSMA-3XQYXDrOUqOencsBXd0_-OQQ',
+    authDomain: 'ai-agent-for-libarry.firebaseapp.com',
+    projectId: 'ai-agent-for-libarry',
+    messagingSenderId: '689128396805',
+    appId: '1:689128396805:web:8b3ece84467982db31646c',
   },
 };
